487382c281b06f7fa5fdeab57ed09dc3ed516953
abdulladone94 <abdulladone94@gmail.com>
2021-10-14 17:53:02 +0530

    Using Axios and fetch data from fake API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-import AppBarMUI from './components/AppBarMUI'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AppBarMUI from "./components/AppBarMUI";
+// import ViewProducts from './components/ViewProducts';
+import axios from "axios";
 
 function App() {
+  const [item, setItem] = useState([]);
+
+   useEffect(
+    () =>
+      axios.get("https://fakestoreapi.com/products").then((response) => {
+        setItem(response.data);
+      }),
+    []
+  );
+
   return (
     <div className="App">
-      <AppBarMUI />     
+      <AppBarMUI />
     </div>
   );
 }
---
+++ src/components/ViewProducts.js
@@ -0,0 +1,9 @@
+// import axios from 'axios' 
+
+// const ViewProducts = () => {
+//     return (
+//         const itemApi = axios.get()
+//     )
+// }
+
+// export default ViewProducts;
